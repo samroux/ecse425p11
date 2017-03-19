9ee5898474af03cfd48801e5a0f3f9749c6e4a18
Po-Shiang <popowang1995@gmail.com>
2017-03-19 01:24:26 -0400

    NEXT_PC = PC + 4; EXE_result  = branch address or jump address; compiled not tested yet

--- PD4_pp/EX/execution.vhd
+++ PD4_pp/EX/execution.vhd
@@ -4,6 +4,7 @@ use ieee.numeric_std.all;
 
 entity execution is
   port(
+   clock: in std_logic;
    instruction: in std_logic_vector(31 downto 0);
 	PC_plus_4: in std_logic_vector(11 downto 0);
 	read_data_1: in std_logic_vector(31 downto 0);
@@ -11,7 +12,7 @@ entity execution is
 	imm: in std_logic_vector(31 downto 0);
 	
 	NEXT_PC: out std_logic_vector(11 downto 0);
-	ALU_result: out std_logic_vector(31 downto 0) := (others => '0')
+	EXE_result: out std_logic_vector(31 downto 0) := (others => '0')
   );
 end execution;
 
@@ -33,6 +34,9 @@ signal ALU_operation: std_logic_vector(3 downto 0);
 signal zero: std_logic;
 
 signal branch_select: std_logic := '0';
+signal ALU_result: std_logic_vector(31 downto 0);
+signal ALU_result_temp: std_logic_vector(31 downto 0);
+signal BorJ: std_logic;
 
 component ALU
 	port(
@@ -76,8 +80,10 @@ ac: alu_control
 		operation => ALU_operation
 	);
 
-process(opcode)
+process(clock)
 begin
+
+if falling_edge(clock) then
     case(opcode) is
         when "000000" => --R type
             Jump <= '0';
@@ -144,19 +150,39 @@ begin
 				Branch <= '0';
 				ALUOp <= "00";
     end case;
+	 
+	 if Branch = '1' then
+		branch_mux <= branch_addr;
+	 elsif Branch = '0' then
+		branch_mux <= std_logic_vector(resize(unsigned(PC_plus_4), 32));
+	 end if;
+	 
+	 if Jump = '1' then
+		ALU_result_temp <= std_logic_vector(resize(unsigned(shift_j_addr), 32));
+	 elsif Jump = '0' then
+		ALU_result_temp <= branch_mux;
+	 end if;
+	 
+	 NEXT_PC <= PC_plus_4;
+end if;
 end process;
 
 shift_j_addr <= std_logic_vector(shift_left(unsigned(jump_addr), 2));
 
-NEXT_PC <= shift_j_addr(11 downto 0) when Jump = '1' else
-	branch_mux(11 downto 0) when Jump = '0' else
-	(others => '0');
+--NEXT_PC <= shift_j_addr(11 downto 0) when Jump = '1' else
+--	branch_mux(11 downto 0) when Jump = '0' else
+--	(others => '0');
 
 shifted_imm <= std_logic_vector(shift_left(unsigned(imm), 2));
 branch_addr <= std_logic_vector(to_signed(to_integer(signed(PC_plus_4)) + to_integer(signed(shifted_imm)), 32));
 
-branch_mux <= branch_addr when Branch = '1' else
-	std_logic_vector(resize(unsigned(PC_plus_4), 32)) when Branch = '0' else
+--branch_mux <= branch_addr when Branch = '1' else
+--	std_logic_vector(resize(unsigned(PC_plus_4), 32)) when Branch = '0' else
+--	(others => '0');
+
+BorJ <= Branch or Jump;
+EXE_result <= ALU_result_temp when BorJ = '1' else
+	ALU_result when BorJ = '0' else
 	(others => '0');
 
 end arch;
